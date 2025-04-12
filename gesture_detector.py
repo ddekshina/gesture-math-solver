00df8d51df673b32811cfa288609b777df0254d0
Devi Dekshina <devidekshina7@gmail.com>
2025-04-13 03:07:32 +0530

    feat: added hand detection using MediaPipe

--- gesture_detector.py
+++ gesture_detector.py
@@ -1,1 +1,16 @@
 #For hand tracking
+import cv2
+import mediapipe as mp
+
+class HandDetector:
+    def __init__(self, max_hands=1):
+        self.hands = mp.solutions.hands.Hands(max_num_hands=max_hands)
+        self.mp_draw = mp.solutions.drawing_utils
+
+    def detect(self, frame):
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = self.hands.process(rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mp_draw.draw_landmarks(frame, hand_landmarks, mp.solutions.hands.HAND_CONNECTIONS)
+        return frame
